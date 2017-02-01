f55ec0d5f333c73500236be35a0e1a0281aa73cf
salotz <salotz@salotz.info>
2017-02-01 16:34:11 -0500

    made setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bibby',
+    version='0.1',
+    py_modules=['bibby'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'Bibsonomy',
+    ],
+    entry_points={
+        'console_scripts' : [
+            "bibby = bibby.cli:cli"]}
+
+)
