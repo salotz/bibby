63295b343d1bb1987b0e0cc7869ff9b5fe6ea475
salotz <salotz@salotz.info>
2017-12-20 15:59:54 -0500

    pybtex dependency, remove_field command
    I added pybtex as a dependency to make it easier to write and parse
    bibtex files in a structured way.
    
    I added a remove_field command that uses pybtex to remove a given
    field.
    
    This really isn't in keeping with the state purpose of bibby and maybe
    it should just be a script but it is in here anyhow for the time being

--- bibby/cli.py
+++ bibby/cli.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
+import sys
 
 import click
+from pybtex.database import parse_file, BibliographyData, Entry
 
 import bibby.bibby as bibby
+
+PYBTEX_FORMAT = 'bibtex'
 
 @click.command()
 @click.option('-v', 'verbosity', default=1, help='verbosity of output')
@@ -25,11 +29,64 @@ def fetch(verbosity, user, token, bib):
     if verbosity > 0:
         print("Saved to {}".format(bib))
 
+
+@click.command()
+@click.option('-O', 'output', default=None, type=click.Path(exists=False),
+              help='File to save modified bibtex file in.')
+@click.option('-v', 'verbosity', default=1, help='verbosity of output')
+@click.argument('bib', nargs=1, type=click.Path(exists=True))
+@click.argument('field', nargs=1, type=str)
+def remove_field(bib, field, output, verbosity):
+    bib_path = bib
+    bib = parse_file(bib)
+    mod_entries = {}
+    for key, entry in bib.entries.items():
+
+        # find it whether it is in fields or persons
+        is_fields = None
+        if field in entry.fields:
+            is_fields = True
+        elif field in entry.persons:
+            is_fields = False
+        else:
+            if verbosity > 0:
+                print("'{}' field not found in the '{}' entry, skipping".format(field, key))
+            mod_entries[entry.key] = entry
+            continue
+
+        fields_dict = dict(entry.fields)
+        persons_dict = dict(entry.persons)
+        # remove the field from the dictionary
+        if is_fields:
+            removed_field = fields_dict.pop(field)
+        else:
+            removed_field = persons_dict.pop(field)
+
+        # recreate the entry and add to the new dictionary of entries
+        new_entry = Entry(entry.type, fields=fields_dict, persons=persons_dict)
+        mod_entries[entry.key] = new_entry
+
+    # make a new bib dataset
+    new_bib = BibliographyData(entries=mod_entries,
+                               preamble=bib.preamble, wanted_entries=bib.wanted_entries)
+    if output is not None:
+        # write to a file
+        new_bib.to_file(output, bib_format=PYBTEX_FORMAT)
+    else:
+        # just send to stdout
+        sys.stdout.write(new_bib.to_string(PYBTEX_FORMAT))
+
+@click.command()
+def abbreviate_field():
+    pass
+
 @click.group()
 def cli():
     pass
 
 cli.add_command(fetch)
+cli.add_command(remove_field)
+cli.add_command(abbreviate_field)
 
 if __name__ == "__main__":
 
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     install_requires=[
         'Click',
         'Bibsonomy',
+        'Pybtex'
     ],
     entry_points={
         'console_scripts' : [
