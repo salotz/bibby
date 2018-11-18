dffe042945b0285a98437b38e74c63ce69e262af
Samuel Lotz <samuel.lotz@salotz.info>
2018-11-18 13:31:31 -0500

    updated the readme and made installable on PyPI

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='bibby',
     version='0.1',
+    author="Samuel D. Lotz",
+    author_email="samuel.lotz@salotz.info",
+    description="Small python library and CLI for doing stuff with Bibsonomy",
     py_modules=['bibby'],
+    url="https://github.com/salotz/bibby",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
